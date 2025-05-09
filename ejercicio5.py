db0a1964707014624c1f31ed07399ab728846ba0
Roberto Fabio Tercero Membreño <rftercerom@uamv.edu.ni>
2025-05-08 21:03:16 -0600

    Ejercicio 5 con validacion y funciones

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,9 +1,23 @@
-num = int(input("Ingresa un n�mero entero positivo: "))
-i = 1
-acumulador = 1
+def calc_factorial(num):     
+     i = 1
+     acumulador = 1
 
-while i <= num:
-     acumulador *= i
-     i+=1
+     while i <= num:
+          acumulador *= i
+          i += 1
 
-print(acumulador)
+     print(f"El factorial de {num} es: {acumulador}")
+
+def main():
+     while True:
+          try:
+               num = int(input("Ingresa un número entero positivo: "))
+               if num <= 0:
+                    raise ValueError
+               break
+          except ValueError:
+               print("ERROR. Tiene que ingresar un número entero y positivo.")
+
+     calc_factorial(num)
+     
+main()
