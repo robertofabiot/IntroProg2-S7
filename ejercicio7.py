ebd75aadf8b84149c8a7cb5f00b534a2b6b4aa83
Roberto Fabio Tercero Membreño <rftercerom@uamv.edu.ni>
2025-05-08 21:21:00 -0600

    Ejercicio 7 con validación y funciones

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,20 +1,30 @@
-""" Enunciado: Pide al usuario una lista de números (pueden ser ingresados uno a la 
+""" Enunciado: Pide al usuario una lista de nï¿½meros (pueden ser ingresados uno a la 
 vez hasta que el usuario ingrese un valor de fin, como 0 o -1). Calcula la suma de 
-los números pares y la suma de los números impares.
-o Especificación: Usa un bucle while, acumuladores para la suma de pares e impares, 
-y un contador (opcional, dependiendo de la implementación)."""
+los nï¿½meros pares y la suma de los nï¿½meros impares.
+o Especificaciï¿½n: Usa un bucle while, acumuladores para la suma de pares e impares, 
+y un contador (opcional, dependiendo de la implementaciï¿½n)."""
 
-x = 0
-suma_pares = 0
-suma_impares = 0
+def calc_pares_impares(x):
+    suma_pares = 0
+    suma_impares = 0
 
-x = int(input("Ingrese un número. Ingrese 0 para finalizar: "))
+    while x != 0:
+        if x % 2 == 0: #numero par
+            suma_pares += x
+        else:
+            suma_impares += x
+        x = int(input("Ingrese un nÃºmero. Ingrese 0 para finalizar: "))
 
-while x != 0:
-    if x % 2 == 0: #numero par
-        suma_pares += x
-    else:
-        suma_impares += x
-    x = int(input("Ingrese un número. Ingrese 0 para finalizar: "))
+    print(f"La suma de los pares es {suma_pares} y la de los impares es {suma_impares}")
 
-print(f"La suma de los pares es {suma_pares} y la de los impares es {suma_impares}")
+def main():
+    while True:
+        try:
+            x = int(input("Ingrese un nÃºmero. Ingrese 0 para finalizar: "))
+            break
+        except ValueError:
+            print("Tiene que ingresar un valor entero")
+            
+    calc_pares_impares(x)
+
+main()
