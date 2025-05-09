cb7bcf0d307c4acc527f601982768f683f1c3fec
Roberto Fabio Tercero Membreño <rftercerom@uamv.edu.ni>
2025-05-08 20:54:04 -0600

    Ejercicio 4 con validacion y funciones

--- ejercicio4.py
+++ ejercicio4.py
@@ -3,11 +3,24 @@ calificación. Calcula el promedio de las calificaciones ingresadas.
 o Especificación: Usa un bucle for para leer las calificaciones, un acumulador para la 
 suma, y un contador para la cantidad."""
 
-n = int(input("Ingrese la cantidad de calificaciones: "))
-acumulador = 0
+def calc_promedio(n):
+    acumulador = 0
 
-for i in range (1, n+1):
-    calificacion = float(input(f"Ingrese la calificación #{i}: "))
-    acumulador += calificacion
+    for i in range (1, n+1):
+        calificacion = float(input(f"Ingrese la calificación #{i}: "))
+        acumulador += calificacion
 
-print(f"El promedio es: {(acumulador/n):.2f}")
+    print(f"El promedio es: {(acumulador/n):.2f}")
+
+def main():
+    while True:
+        try:
+            n = int(input("Ingrese la cantidad de calificaciones: "))
+            if n <= 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Tiene que ingresar un número entero positivo.")
+    calc_promedio(n)
+
+main()
