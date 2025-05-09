635ca219468d17ffb40bdf7ae02b972097959b7f
Roberto Fabio Tercero Membreño <rftercerom@uamv.edu.ni>
2025-05-08 20:16:42 -0600

    Ejercicio 1 con validación y funciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,22 @@
-num = int(input("Ingrese un numero entero positivo: "))
-suma = 0
+"""Suma de los primeros N números 
+o Enunciado: Pide al usuario un número entero positivo N y calcula la suma de los 
+números desde 1 hasta N. 
+o Especificación: Usa un bucle for y un acumulador."""
 
-for i in range (1, num+1):
-    suma += i
+def calc_suma(num):
+    suma = 0
+    for i in range (1, num+1):
+        suma += i
+    return suma
 
-print(f"La suma es {suma}")
+def main():
+    while True:
+        try:
+            num = int(input("Ingrese un número entero: "))
+            num = abs(num)
+            break
+        except ValueError:
+            print("Tiene que ingresar un número entero.")
+    print(f"La suma es {calc_suma(num)}")
+    
+main()
