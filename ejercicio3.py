6292a96cb95d694d8d71f291444d64b69e01963a
Roberto Fabio Tercero Membreño <rftercerom@uamv.edu.ni>
2025-05-08 20:50:47 -0600

    Ejercicio 3 con funciones

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,12 +2,18 @@
 o Especificación: Usa un bucle for para recorrer la cadena y contadores para cada 
 vocal."""
 
-cadena = input("Ingrese un texto: ")
-contador = 0
-cadena = cadena.lower()
+def contar_vocales(cadena):
+    contador = 0
+    cadena = cadena.lower()
 
-for caracter in cadena:
-    if caracter == "a" or caracter == "e" or caracter == "i" or caracter == "o" or caracter == "u":
-        contador += 1
+    for caracter in cadena:
+        if caracter == "a" or caracter == "e" or caracter == "i" or caracter == "o" or caracter == "u":
+            contador += 1
 
-print(f"Las vocales en el texto son: {contador}")
+    print(f"Las vocales en el texto son: {contador}")
+
+def main():
+    cadena = input("Ingrese un texto: ")
+    contar_vocales(cadena)
+
+main()
